87b171daf35ea00e8da9e7341ebc800c70b4f15b
Ryan Havens <ryan.d.havens@gmail.com>
2016-10-14 22:20:07 -0600

    commenting out ntlk stuff

--- app.py
+++ app.py
@@ -40,43 +40,43 @@ def send_img(path):
 #     # add client fb info to user 
 
 
-# bootstrapping the NLTK
-from nltk.classify import NaiveBayesClassifier
-from nltk.corpus import subjectivity
-from nltk.sentiment import SentimentAnalyzer
-from nltk.sentiment.util import *
+# # bootstrapping the NLTK
+# from nltk.classify import NaiveBayesClassifier
+# from nltk.corpus import subjectivity
+# from nltk.sentiment import SentimentAnalyzer
+# from nltk.sentiment.util import *
 
-n_instances = 100
-subj_docs = [(sent, 'subj') for sent in subjectivity.sents(categories='subj')[:n_instances]]
-obj_docs = [(sent, 'obj') for sent in subjectivity.sents(categories='obj')[:n_instances]]
+# n_instances = 100
+# subj_docs = [(sent, 'subj') for sent in subjectivity.sents(categories='subj')[:n_instances]]
+# obj_docs = [(sent, 'obj') for sent in subjectivity.sents(categories='obj')[:n_instances]]
 
-train_subj_docs = subj_docs[:80]
-test_subj_docs = subj_docs[80:100]
-train_obj_docs = obj_docs[:80]
-test_obj_docs = obj_docs[80:100]
-training_docs = train_subj_docs + train_obj_docs
-testing_docs = test_subj_docs + test_obj_docs
+# train_subj_docs = subj_docs[:80]
+# test_subj_docs = subj_docs[80:100]
+# train_obj_docs = obj_docs[:80]
+# test_obj_docs = obj_docs[80:100]
+# training_docs = train_subj_docs + train_obj_docs
+# testing_docs = test_subj_docs + test_obj_docs
 
-sentim_analyzer = SentimentAnalyzer()
-all_words_neg = sentim_analyzer.all_words([mark_negation(doc) for doc in training_docs])
+# sentim_analyzer = SentimentAnalyzer()
+# all_words_neg = sentim_analyzer.all_words([mark_negation(doc) for doc in training_docs])
 
-unigram_feats = sentim_analyzer.unigram_word_feats(all_words_neg, min_freq=4)
-sentim_analyzer.add_feat_extractor(extract_unigram_feats, unigrams=unigram_feats)
+# unigram_feats = sentim_analyzer.unigram_word_feats(all_words_neg, min_freq=4)
+# sentim_analyzer.add_feat_extractor(extract_unigram_feats, unigrams=unigram_feats)
 
-training_set = sentim_analyzer.apply_features(training_docs)
-test_set = sentim_analyzer.apply_features(testing_docs)
+# training_set = sentim_analyzer.apply_features(training_docs)
+# test_set = sentim_analyzer.apply_features(testing_docs)
 
-trainer = NaiveBayesClassifier.train
-classifier = sentim_analyzer.train(trainer, training_set)
+# trainer = NaiveBayesClassifier.train
+# classifier = sentim_analyzer.train(trainer, training_set)
 
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from nltk import tokenize
+# from nltk.sentiment.vader import SentimentIntensityAnalyzer
+# from nltk import tokenize
 
-sid = SentimentIntensityAnalyzer()
+# sid = SentimentIntensityAnalyzer()
 
-def sentiment_analyze(sentence):
-	print sentence
-	ss = sid.polarity_scores(sentence)
-	for k in sorted(ss):
-		print('{0}: {1}, '.format(k, ss[k]))
-	print
+# def sentiment_analyze(sentence):
+# 	print sentence
+# 	ss = sid.polarity_scores(sentence)
+# 	for k in sorted(ss):
+# 		print('{0}: {1}, '.format(k, ss[k]))
+# 	print
