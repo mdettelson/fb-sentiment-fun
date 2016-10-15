0e4ed42472d1762585b793eae432a6697a57f40a
Ryan Havens <ryan.d.havens@gmail.com>
2016-10-14 22:24:40 -0600

    CODING WOO

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ def send_css(path):
 def send_img(path):
 	return send_from_directory('img', path)
 
-# if __name__ == "__main__":
-    # app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
 
 # @app.route('/login')
 # def login(user):
